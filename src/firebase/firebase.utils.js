c0da2064d44d468bcc8eb9b2fea6b81f6925190f
Justin <therealmathhelper@gmail.com>
2020-08-01 23:18:57 -0400

    added ability to retrieve shop  information from firestore using asynchronous method

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,16 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyA2BISIPyNh0FH6M1BbW7GBnkbC-BqbvEM",
+  authDomain: "lampros-clothing.firebaseapp.com",
+  databaseURL: "https://lampros-clothing.firebaseio.com",
+  projectId: "lampros-clothing",
+  storageBucket: "lampros-clothing.appspot.com",
+  messagingSenderId: "1009577666878",
+  appId: "1:1009577666878:web:c559e2ae06edabfdd0a46f",
+  measurementId: "G-4M10463JY1"
 };
+
 
 firebase.initializeApp(config);
 
@@ -20,6 +22,8 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   const userRef = firestore.doc(`users/${userAuth.uid}`);
 
   const snapShot = await userRef.get();
+
+
 
   if (!snapShot.exists) {
     const { displayName, email } = userAuth;
@@ -38,6 +42,35 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
   return userRef;
 };
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = firestore.collection(collectionKey);
+  const batch = firestore.batch();
+
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+}
+
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routename: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    }
+  })
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+}
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -13,6 +13,7 @@ import {
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
+  console.log("collectionPage was called");
   return (
     <CollectionPageContainer>
       <CollectionTitle>{title}</CollectionTitle>
